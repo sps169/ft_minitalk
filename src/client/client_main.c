75b5851d0b0f5705d03e136a64c1108a609ac881
Sergio Perez sanz <sperez-s@c3r4s6.42madrid.com>
2022-10-25 10:32:33 +0200

    updated error msgs client

--- src/client/client_main.c
+++ src/client/client_main.c
@@ -6,7 +6,7 @@
 /*   By: sperez-s <sperez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:26:24 by sperez-s          #+#    #+#             */
-/*   Updated: 2022/10/25 10:07:23 by sperez-s         ###   ########.fr       */
+/*   Updated: 2022/10/25 10:30:25 by sperez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ int	main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		pid = ft_atoi(argv[1]);
-		if (pid > 0 && argv[2] && ft_strlen(argv[2]) > 0)
-			send_string_to_server(argv[2], pid);
+		if (pid > 0)
+		{
+			if (ft_strlen(argv[2]) > 0)
+				send_string_to_server(argv[2], pid);
+			else
+				ft_printf("Empty string\n");
+		}
 		else
-			ft_printf("Wrong args format\n");
+			ft_printf("Wrong pid\n");
 	}
 	else
 		ft_printf("Wrong args\n");
